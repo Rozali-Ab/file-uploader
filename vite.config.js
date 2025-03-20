ffd348fc2ea3aed2115fbc4a6f056e450142bb68
Rozali-Ab <rozali_ab@outlook.com>
2025-03-20 06:20:52 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default {
   base: '/file-uploader/',
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://your-default-api-url.com')
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://file-upload-server-mc26.onrender.com/api/v1/upload')
   }
 } 
